52257c88966d2a7cf23f978669a48769380f862f
kafq <kafque@gmail.com>
2021-12-29 17:03:48 +0200

    Add variant switching

--- src/helpers/populateByName.ts
+++ src/helpers/populateByName.ts
@@ -1,26 +1,58 @@
+const delimiter = ", "
+
 const populateByName = (selectedLayers, JSONobj, selectedItem) => {
-	let newItem = 0
+	let layerCount = 0
 
 	const loopSelected = arr => {
 		arr.map(item => {
+			const JSONItemVal = JSONobj[layerCount]?.[selectedItem]
 			if (
 				item.name.toUpperCase() === selectedItem.toUpperCase() &&
-				item.type === "TEXT"
+				item.type === "TEXT" &&
+				JSONItemVal?.toString().split("-")[0] !== "VARIANT"
 			) {
 				figma.loadFontAsync(item.fontName).then(() => {
-					if (typeof JSONobj[newItem] !== "undefined") {
-						item.characters = JSONobj[newItem]?.[selectedItem]?.toString()
-						newItem = ++newItem
+					if (typeof JSONobj[layerCount] !== "undefined") {
+						item.characters = JSONItemVal.toString()
+						layerCount = ++layerCount
 					}
 				})
 			}
 
 			if (
 				item.name.toUpperCase() === selectedItem.toUpperCase() &&
-				item.type !== "TEXT"
+				item.type !== "TEXT" &&
+				typeof JSONItemVal === "boolean"
 			) {
-				item.visible = JSONobj[newItem]?.[selectedItem] ? true : false
-				newItem = ++newItem
+				item.visible = JSONobj[layerCount]?.[selectedItem] ? true : false
+				layerCount = ++layerCount
+			}
+
+			if (
+				item.name.toUpperCase() === selectedItem.toUpperCase() &&
+				item.mainComponent?.parent?.type === "COMPONENT_SET" &&
+				JSONItemVal?.toString().split("-")[0] === "VARIANT"
+			) {
+				let propertyIndex = -1
+				const propertyName = JSONItemVal.split("-")[1]
+				const nodeProperties = item.mainComponent.name.split("-")
+				propertyIndex = nodeProperties.findIndex(i =>
+					i.startsWith(`${propertyName}=`)
+				)
+				const propertyValue = JSONItemVal.split("-")[2]
+
+				if (propertyIndex !== -1) {
+					nodeProperties[propertyIndex] = `${propertyName}=${propertyValue}`
+					const newVariantName = nodeProperties.join("")
+					const newVariant = item.mainComponent.parent.findChild(
+						i => i.name === newVariantName
+					)
+					if (newVariant) {
+						console.log("Attempting swap")
+						item.swapComponent(newVariant)
+						layerCount = ++layerCount
+					}
+				}
 			}
 
 			if (item.children) {
